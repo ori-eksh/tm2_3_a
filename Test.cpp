6a487a08b7f27dff711ab4d0d7821f932c0d030e
Ori <mymail@gmail.com>
2023-04-17 20:01:05 +0300

    update

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,10 @@
 #include "doctest.h"
 #include <stdexcept>
 #include "sources/Fraction.hpp"
+#include <iostream>
+#include <algorithm>
+// #include <typeinfo>
+#include <sstream>
 using namespace ariel;
 
 TEST_CASE("the operator +")
@@ -142,4 +146,24 @@ TEST_CASE("the operator --")
 
     CHECK((--num1).getDenominator() == 10);
     CHECK((--num1).getNumerator() == 3);
+}
+TEST_CASE("the operator <<")
+{
+    Fraction num1(7, 6);
+    Fraction num2(32, 6);
+    std::ostringstream print;
+    CHECK_NOTHROW(print << num1);
+    CHECK(print.str() == "7/6");
+    CHECK_THROWS(print << "num2");
+}
+TEST_CASE("the operator >>")
+{
+    Fraction num1(0, 1), num2(0, 1);
+    istringstream good_num("9 10");
+    istringstream bad_num("1");
+
+    CHECK_NOTHROW(good_num >> num1);
+    CHECK(num1.getNumerator() == 9);
+    CHECK(num1.getDenominator() == 10);
+    CHECK_THROWS(bad_num >> num2);
 }
